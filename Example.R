ab5652236222d005ff69e8082c514600b228dffe
Jose Eugenio Lozano Alonso <LozAloJo@jcyl.red>
2018-03-28 10:00:34 +0200

    Initial release

--- Example.R
+++ Example.R
@@ -18,43 +18,33 @@ library(doParallel)
 library(lubridate)
 library(stringi)
 
+### Set parameters
+# List of profiles, a list of lists with names and usernames (can be more than one)
+profile.names.list<-list(
+  list(name="Jose E. Lozano", lastfm.users="jelozano")
+)
+# Should the program restart from the begining or continue if the program failed due to timeouts?
+restart.tasks=F
+# Working directory
+wd.dir <- "C:/lastfm-analyser-data"
+
 ### Set the working directory
-setwd("C:/JLozano/lastfm-analyser-data")
+if (!dir.exists(wd.dir)) dir.create(wd.dir)
+setwd(wd.dir)
 
 ### Sep API key for lastfm, you have to create one at https://www.last.fm/es/api 
 api_key="fa88980a5ebb1dc8d3375a0451c1b5fb"
 
 ### Download required functions from github
-
 if (!dir.exists("programs")) dir.create("programs")
-# https://github.com/ppatrzyk/lastfm-to-R
+# https://github.com/lozalojo/lastfm-to-R, forged from: https://github.com/ppatrzyk/lastfm-to-R
 if (!file.exists("programs/lastfm.R")) download.file("https://raw.githubusercontent.com/lozalojo/lastfm-to-R/master/lastfm.R", "programs/lastfm.R")
+# https://github.com/lozalojo/lastfm-analyser
+if (!file.exists("programs/lastfm-analyser.R")) download.file("https://raw.githubusercontent.com/lozalojo/lastfm-analyser/master/lastfm-analyser.R", "programs/lastfm-analyser.R")
+
+### Source functions
 source("programs/lastfm.R")
-# https://github.com/ppatrzyk/lastfm-to-R
-if (!file.exists("programs/lastfm-analyser.R")) download.file("https://raw.githubusercontent.com/lozalojo/lastfm-to-R/master/lastfm-analyser.R", "programs/lastfm-analyser.R")
 source("programs/lastfm-analyser.R")
-
-
-
-profile.names.list<-list(
-  list(name="@corpcd", lastfm.users="jelozano"),
-  list(name="@Kivenkantaja", lastfm.users="l_mou_l"),
-  list(name="@Rider of storm", lastfm.users=c("rider_of_Storm","seitan_mb")),
-  list(name="Neutravo", lastfm.users="Neutravo"),
-  list(name="truenoacero", lastfm.users="truenoacero"),
-  list(name="reyox", lastfm.users="reyox"),
-  list(name="@Aristóteles", lastfm.users="WoodenDors"),
-  list(name="@mattogrosso", lastfm.users="mattogrosso"),
-  list(name="@metalgod", lastfm.users="tuviejapelleja"),
-  list(name="@El_Tolo", lastfm.users="el_tolo")
-)
-
-restart.tasks=F
-
-source("programs/analyser - functions.R")
-
-# i<-1
-# temp1<-lastfm.analiser(i.profile.name=profile.names.list[[i]], i.api_key=api_key, i.restart=restart.tasks)
 
 lastfm.results<-list()
 for (i in 1:length(profile.names.list)) lastfm.results[[i]]<-lastfm.analiser(i.profile.name=profile.names.list[[i]], i.api_key=api_key, i.restart=restart.tasks)
